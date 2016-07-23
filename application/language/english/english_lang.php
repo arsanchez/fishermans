4db90347eccaa8d6b5ea4d903ab9d80cb9c3c7ec
Argenis Sánchez <argenissanchez@Argeniss-MacBook-Pro.local>
2016-07-23 00:29:29 -0400

    Set up commit

--- application/language/english/english_lang.php
+++ application/language/english/english_lang.php
@@ -3,5 +3,10 @@
 #
 # General
 $lang['test'] = "English test";
+$lang['page_title'] = "Fishermans";
+$lang['new_ship_title'] = "New ship";
+$lang['owners_list'] = "Owners list";
+$lang['owners'] = "Owners";
+
 /* STOP TRANSLATING */
 ?>
--- application/language/spanish/spanish_lang.php
+++ application/language/spanish/spanish_lang.php
@@ -3,5 +3,9 @@
 #
 # General
 $lang['test'] = "Prueba en esp";
+$lang['page_title'] = "Fishermans";
+$lang['owners_list'] = "Lista de dueños";
+$lang['new_ship_title'] = "Nueva embarcaci&oacute;n";
+$lang['owners'] = "Dueños";
 /* STOP TRANSLATING */
 ?>
